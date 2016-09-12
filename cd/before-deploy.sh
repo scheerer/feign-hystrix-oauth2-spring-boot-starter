90d7edd960f0b83e21338b6df60f3eadb1c8c884
Jason Al-Mansor <jason.al-mansor@nextgearcapital.com>
2016-09-12 14:21:46 -0400

    adding necessary keys

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+	openssl aes-256-cbc -K $encrypted_7b4956c049f5_key -iv $encrypted_7b4956c049f5_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
