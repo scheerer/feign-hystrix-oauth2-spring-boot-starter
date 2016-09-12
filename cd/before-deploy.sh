3ebbbbf21163b6f60d3bd063f51b47c53b644503
Jason Al-Mansor <jason.al-mansor@nextgearcapital.com>
2016-09-12 15:02:33 -0400

    fix filename reference

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_7b4956c049f5_key -iv $encrypted_7b4956c049f5_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import cd/codesigning.asc
 fi
