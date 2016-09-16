84cb2d4eb8907990b5c435f97db387e0bbd67059
Jason Al-Mansor <jalmansor@gmail.com>
2016-09-16 16:12:19 -0400

    installing correct package

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_799e87808ef3_key -iv $encrypted_799e87808ef3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpgv2 --fast-import cd/codesigning.asc
+    gpg2 --fast-import cd/codesigning.asc
 fi
