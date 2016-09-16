66e3fe9e9805301f6f5ef097a5b7cc6cbde5b4ae
Jason Al-Mansor <jalmansor@gmail.com>
2016-09-16 15:19:56 -0400

    updating signing keys

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_7b4956c049f5_key -iv $encrypted_7b4956c049f5_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_799e87808ef3_key -iv $encrypted_799e87808ef3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
